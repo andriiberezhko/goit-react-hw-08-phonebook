7be418257fc7d677a990ee50d1bdfda422ff9409
Andrii Berezhko <Berezhko7777@ukr.net>
2022-08-30 15:26:51 +0300

    +

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,6 +4,6 @@ import authSelectors from 'redux/auth/auth-selectors';
 
 export default function PrivateRoute() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  console.log('Private', isLoggedIn);
+  // console.log('Private', isLoggedIn);
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
 }
